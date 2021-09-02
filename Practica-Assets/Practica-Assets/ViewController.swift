51b250b6eb71c491c23df24450b7c1a947ec31d5
Carlos Encinas <53828095+iCarlosEncinas@users.noreply.github.com>
2021-09-01 22:38:31 -0700

    Add files via upload

---
+++ Practica-Assets/Practica-Assets/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Practica-Assets
+//
+//  Created by David Encinas on 01/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imgSegundaImagen: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imgSegundaImagen.image = UIImage(named: "bola amarilla miada equisde mocos")
+
+    }
+    
+
+}
+
